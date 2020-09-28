3655098c439a6a1bf5cdbfa7cf412ab0131310e0
Zsolt Toth <tothzs87@gmail.com>
2020-09-28 09:44:32 +0200

    OOP Fundamentals tasks.

---
+++ oop/oop-fundamentals/src/main/java/hu/uni/eke/multiparadigm/oop/App.java
@@ -0,0 +1,17 @@
+package hu.uni.eke.multiparadigm.oop;
+
+/**
+ * Tasks 1: Create a Solver for Quadratic Equations. Coefficients are set manually. Console read is not necessary.
+ *
+ * Task 2: Implement the BubbleSort to sort double arrays. Overload the implementation so it could order the elements in ASC or DESC.
+ *
+ * Task 3: Compare OOP implementation to the procedural-programming example.
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
