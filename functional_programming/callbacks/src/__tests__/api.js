ab78df5bc03542ce797fed1cd497d88a16357d1b
Zsolt Toth <tothzs87@gmail.com>
2020-11-17 11:07:06 +0100

    Grading tests and function stubs are added.

--- functional_programming/callbacks/src/__tests__/api.js
+++ functional_programming/callbacks/src/__tests__/api.js
@@ -89,3 +89,107 @@ describe('Array Comprehension Tests', ()=>{
     })
 
 });
+
+describe('Grading Tests', ()=>{
+
+    const FAILED_EXAM_RESULT = 'FAILED';
+    const PASSED_EXAM_RESULT = 'PASSED';
+    const exam_results = [
+        {
+            name : 'Alice',
+            score : 32
+        },
+        {
+            name : 'Bob',
+            score : 39
+        },
+        {
+            name : 'Charlie',
+            score : 98
+        },
+        {
+            name : 'Denis',
+            score : 78
+        },
+        {
+            name : 'Eve',
+            score : 56
+        },
+        {
+            name : 'Frank',
+            score : 14
+        },
+        {
+            name : 'Gabe',
+            score : 49
+        },
+        {
+            name : 'Hugo',
+            score : 39
+        },
+        {
+            name : 'Ivan',
+            score : 100
+        },
+        {
+            name : 'John',
+            score : 43
+        },
+        {
+            name : 'Kevin',
+            score : 65
+        },
+        {
+            name : 'Lousie',
+            score : 54
+        },
+        {
+            name : 'Maria',
+            score : 63
+        },
+        {
+            name : 'Nate',
+            score : 65
+        },
+        {
+            name : 'Oliver',
+            score : 12
+        },
+        {
+            name : 'Pedro',
+            score : 87
+        },
+        {
+            name : 'Rebeca',
+            score : 83
+        },
+        {
+            name : 'Sarah',
+            score : 29
+        },
+        {
+            name : 'Tamara',
+            score : 73
+        },
+        {
+            name : 'Wade',
+            score : 5
+        },
+    ]
+
+    it('Grade A failed Student', ()=>{
+        const expected = {...exam_results[0], result : FAILED_EXAM_RESULT};
+        const actual = api.grade_students(exam_results)[0];
+        expect(actual).toEqual(expected);
+    })
+
+    it('Grade A passed Student', ()=>{
+        const INDEX_OF_CHARLIE_WHO_PASSED = 2;
+        const expected = {...exam_results[INDEX_OF_CHARLIE_WHO_PASSED], result : FAILED_EXAM_RESULT};
+        const actual = api.grade_students(exam_results)[INDEX_OF_CHARLIE_WHO_PASSED];
+        expect(actual).toEqual(expected);
+    })
+
+
+
+});
--- functional_programming/callbacks/src/api.js
+++ functional_programming/callbacks/src/api.js
@@ -12,17 +12,34 @@ const _sum_of_array = (array = []) => {
 }
 
 const _count_positives = (array = []) => {
-    return 0;
+    return _filter_positives(array).length;
 }
 
 const _filter_positives = (array = []) => {
-    return [];
+    return array.filter(num => num >= 0.0);
 }
 
 const _power = (array = []) => {
-    return [];
+    return array.map(num => num * num);
 }
 
+const _grade_students = (exam_results, threshold = 50) => {
+    return [{name : 'Alice', score: 32, result : 'FAILED'}];
+}
+
+const _avg_score = (exam_results) => {
+    return 0;
+}
+
+const _std_score = (exam_results) => {
+    return 1.0;
+}
+
+const _fit_results_to_curve = (exam_results, standard_mean = 50, standard_std = 10) => {
+    const student_score = 32;
+    let standard_score = (((_avg_score(exam_results) - student_score) / _std_score(exam_results)) * standard_std) + standard_mean;
+    return [{name : 'Alice', score: student_score, standard_score : standard_score}];
+}
 
 module.exports = {
     dummy_function : dummy_function,
@@ -30,5 +47,8 @@ module.exports = {
     sum_of_array : _sum_of_array,
     count_positives : _count_positives,
     filter_positives : _filter_positives,
-    power : _power
+    power : _power,
+    grade_students : _grade_students,
+    avg_score : _avg_score,
+    std_score : _std_score
 }
