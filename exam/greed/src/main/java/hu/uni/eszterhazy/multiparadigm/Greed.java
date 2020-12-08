3fac561bc046c0778783a41b25ef8022f941cea1
Zsolt Toth <tothzs87@gmail.com>
2020-12-08 10:40:57 +0100

    feat (greed) : Greed checkDice was outsourced into a private method and magic numbers were eliminated.

--- exam/greed/src/main/java/hu/uni/eszterhazy/multiparadigm/Greed.java
+++ exam/greed/src/main/java/hu/uni/eszterhazy/multiparadigm/Greed.java
@@ -4,19 +4,26 @@ import java.util.Arrays;
 
 public class Greed {
 
+    private static final int MINIMUM_DICE_COUNT = 1;
+    private static final int MAXIMUM_DICE_COUNT = 6;
+    private static final int MINIMUM_DICE_VALUE = 1;
+    private static final int MAXIMUM_DICE_VALUE = 6;
 
     public int score(int[] dice) throws IllegalArgumentException{
-        if(dice.length < 1 || dice.length > 6){
-            throw new IllegalArgumentException();
-        }
-        Arrays.stream(dice).forEach(die -> {
-            if((die < 1) || (die > 6)){
-                throw new IllegalArgumentException();
-            }
-        });
+        checkDice(dice);
         return 0;
     }
 
+    private void checkDice(int[] dice) throws  IllegalArgumentException{
+        if(dice.length < MINIMUM_DICE_COUNT || dice.length > MAXIMUM_DICE_COUNT){
+            throw new IllegalArgumentException();
+        }
+        Arrays.stream(dice).forEach(die -> {
+            if((die < MINIMUM_DICE_VALUE) || (die > MAXIMUM_DICE_VALUE)){
+                throw new IllegalArgumentException();
+            }
+        });
+    }
 
 
 }
