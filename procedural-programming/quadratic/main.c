ecf65793a2598c97be00eb53c78729e1e29d3519
Zsolt Toth <tothzs87@gmail.com>
2021-09-11 13:56:06 +0200

    refactor(c): delete sorting from quadratic project

--- procedural-programming/quadratic/main.c
+++ procedural-programming/quadratic/main.c
@@ -1,43 +1,11 @@
 #include <stdio.h>
 #include "quadratic.h"
-#include "sort.h"
-
-/*
- * Tasks:
- *  - Refactor code
- *  - Discuss Quadratic and Sort functions.
- *  - Discuss Function Pointer
- */
-
-int decrease(double a, double b){
-    return a < b ? 1 : 0;
-}
 
 int main() {
     printf("Hello, World!\n");
     double x1, x2;
     int resp = solve(1,4, 4,&x1,&x2);
     printf("Response: %d, (%lf,%lf)\n",resp,x1,x2);
-
-    double a[5] = {1,4,-2, 3, 9};
-    int i;
-
-    for(i = 0; i < 5; i++){
-        printf("%d: %f\n", i, a[i]);
-    }
-    printf("\n----------\n");
-    bubbleSort(&a, 5);
-    for(i = 0; i < 5; i++){
-        printf("%d: %f\n", i, a[i]);
-    }
-
-    printf("\n----------\n");
-    double a2[] = {1,4,-2, 3, 9};
-    bubbleSortEnhanced(&a2, 5, decrease);
-    for(i = 0; i < 5; i++){
-        printf("%d: %f\n", i, a2[i]);
-    }
-
 
     return 0;
 }
