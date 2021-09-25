6debb6e5f301d8b5b894d24326eb0486caab9736
Zsolt Toth <tothzs87@gmail.com>
2021-09-25 12:44:32 +0200

    feat(oop): add array-splitting project template

---
+++ oop/array-splitting/src/main/java/hu/ekcu/java/App.java
@@ -0,0 +1,24 @@
+package hu.ekcu.java;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+
+@SpringBootApplication
+public class App implements CommandLineRunner {
+
+    @Autowired
+    ApplicationContext context;
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
