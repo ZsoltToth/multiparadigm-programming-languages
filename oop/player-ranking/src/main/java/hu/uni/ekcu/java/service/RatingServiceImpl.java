062296e85eee4a08a2e59f97fd5247cc29c7b7e9
Zsolt Toth <tothzs87@gmail.com>
2021-11-24 11:26:14 +0100

    test(player-ranking): update RatingServiceImplTest to use eloRatingCalculator

--- oop/player-ranking/src/main/java/hu/uni/ekcu/java/service/RatingServiceImpl.java
+++ oop/player-ranking/src/main/java/hu/uni/ekcu/java/service/RatingServiceImpl.java
@@ -2,24 +2,27 @@ package hu.uni.ekcu.java.service;
 
 import hu.uni.ekcu.java.model.Player;
 import hu.uni.ekcu.java.persist.PlayerRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RatingServiceImpl implements RatingService {
 
     private final PlayerRepository playerRepository;
 
-    private final int adjustment;
+    private final EloRatingCalculator eloRatingCalculator;
 
-    public RatingServiceImpl(PlayerRepository playerRepository, int adjustment) {
-        this.playerRepository = playerRepository;
-        this.adjustment = adjustment;
-    }
 
     @Override
     public void recordGameResult(Player winner, Player loser) {
-        winner.setScore(winner.getScore() + adjustment);
-        loser.setScore(loser.getScore() - adjustment);
+        EloRatingCalculator.Scores updatedScores = eloRatingCalculator.calculate(
+            new EloRatingCalculator.Scores(
+                winner.getScore(),
+                loser.getScore()
+            ));
+        winner.setScore(updatedScores.getWinner());
+        loser.setScore(updatedScores.getLoser());
         playerRepository.save(winner);
         playerRepository.save(loser);
     }
--- oop/player-ranking/src/test/java/hu/uni/ekcu/java/service/RatingServiceImplTest.java
+++ oop/player-ranking/src/test/java/hu/uni/ekcu/java/service/RatingServiceImplTest.java
@@ -2,14 +2,14 @@ package hu.uni.ekcu.java.service;
 
 import hu.uni.ekcu.java.model.Player;
 import hu.uni.ekcu.java.persist.PlayerRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.any;
 
 @ExtendWith(MockitoExtension.class)
 class RatingServiceImplTest {
@@ -17,14 +17,12 @@ class RatingServiceImplTest {
     @Mock
     private PlayerRepository repository;
 
-    private static final int ADJUSTMENT = 5;
+    @Mock
+    private EloRatingCalculator eloRatingCalculator;
 
+    @InjectMocks
     private RatingServiceImpl service;
 
-    @BeforeEach
-    public void setUp(){
-        service = new RatingServiceImpl(repository, ADJUSTMENT);
-    }
 
 
     @Test
@@ -35,7 +33,7 @@ class RatingServiceImplTest {
 
         Player winnerUpdated = new Player("winner", 15);
         Player loserUpdated = new Player("loser", 5);
-
+        when(eloRatingCalculator.calculate(any())).thenReturn(new EloRatingCalculator.Scores( 15,5));
         when(repository.save(winnerUpdated)).thenReturn(winnerUpdated);
         when(repository.save(loserUpdated)).thenReturn(loserUpdated);
        // when
